4e246be0f7d2a213c3bbc245df7453f1d1e7dd62
leonardo silva <silvaleonardo.ti@gmail.com>
2020-10-28 21:27:16 -0300

    Using Axios to fetch GitHub API

---
+++ JavaScript/JS_Assincrono/main.js
@@ -0,0 +1,35 @@
+// let minhaPromisse = function(){
+//     return new Promise(function(resolve, reject){
+//         let xhr = XMLHttpRequest()
+
+//         xhr.open('GET', 'https://api.github.com/users/leosilva-dev')
+//         xhr.send(null)
+
+//         xhr.onreadystatechange = function(){
+//         if(xhr.readtstate === 4){
+//             if(xhr.status === 200){
+//                 resolve(JSON.parse(chr.responseText))
+//             }else{
+//                 reject("Erro na requisição")
+//             }
+        
+//     }
+// }
+//     })
+// }
+
+// minhaPromisse()
+//     .then(function(response){
+//         console.log(response)
+//     })
+//     .catch(function(error){
+//         console.warn(error)
+//     })
+
+axios.get('https://api.github.com/users/leosilva-dev')
+    .then(function(response){
+        console.log(response)
+    })
+    .catch(function(reject){
+        console.warn(error)
+    })
