3055767c33735b74e4420442885b1ce877920891
leonardo silva <silvaleonardo.ti@gmail.com>
2020-11-30 22:52:53 -0300

    Finalizado exercicios parte 3

--- ES6/Challenges/03.js
+++ ES6/Challenges/03.js
@@ -8,4 +8,48 @@ arr.map(function(item) {
 
 const arr2 = arr.map(item => item + 10);
 
-console.log(arr2)
+// console.log(arr2)
+
+//-----------------------------------------
+
+// 3.2
+// Dica: Utilize uma constante pra function
+const usuario = { nome: 'Diego', idade: 23 };
+
+// function mostraIdade(usuario) {
+    //  return usuario.idade;
+    // }
+    
+    // mostraIdade(usuario);
+    
+    
+const showAge = () => usuario.idade
+    
+    
+//-----------------------------------------
+
+// 3.3
+// Dica: Utilize uma constante pra function
+const nome = "Diego";
+const idade = 23;
+function mostraUsuario(nome = 'Diego', idade = 18) {
+ return { nome, idade };
+}
+mostraUsuario(nome, idade);
+mostraUsuario(nome);
+
+
+const showUser = (nome = 'Diego', idade = 18) => {
+    return {nome, idade}
+} 
+
+//-----------------------------------------
+
+// 3.4
+const promise = function() {
+    return new Promise(function(resolve, reject) {
+    return resolve();
+    })
+   }
+
+const promise2 = () => new Promise ((resolve, reject)=> resolve()) 
